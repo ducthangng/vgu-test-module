27a2701ecc7c1872617de5136f1c8a4d0ff23082
Duc Thang Nguyen <nickworkspace189@gmail.com>
2022-05-18 17:47:45 +0700

    Add Routes, Layouts, Guard. [+] Add Routing. [+] Add Guard for routing. [+] Add Application NavBar. [+] Add Layout for users and classroom.

---
+++ ui/src/pages/Layout/UserLayout.tsx
@@ -0,0 +1,99 @@
+import { Layout, Menu } from 'antd';
+import React, { Children } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+import {
+  HomeOutlined,
+  AppstoreOutlined,
+  BellOutlined,
+  UserOutlined,
+  LogoutOutlined,
+  BookOutlined,
+} from '@ant-design/icons';
+import '../../App.scss';
+
+const { Content, Header } = Layout;
+
+const UserLayout = () => {
+  let navigate = useNavigate();
+
+  const defaultSelectedKeys = () => {
+    let pathname = window.location.pathname;
+    if (pathname === '/') {
+      return ['1'];
+    } else if (pathname === '/classroom') {
+      return ['2'];
+    } else if (pathname === '/notification') {
+      return ['3'];
+    } else if (pathname === '/account') {
+      return ['4'];
+    } else {
+      return ['1'];
+    }
+  };
+
+  return (
+    <>
+      <Layout className="App">
+        <Header
+          className="AppHeader"
+          style={{
+            width: '100%',
+            position: 'fixed',
+            display: 'flex',
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            padding: '0',
+            zIndex: 2,
+            backgroundColor: 'white',
+            boxShadow: '0px 2px 5px #ccc',
+            top: '0',
+            paddingLeft: '8vw',
+          }}
+        >
+          <a onClick={() => navigate('/')}>
+            <img src="https://picsum.photos/200" width="30px"></img>
+          </a>
+          <div className="ItemMenu">
+            <Menu
+              defaultSelectedKeys={defaultSelectedKeys()}
+              defaultOpenKeys={['sub1']}
+              mode={'horizontal'}
+              _internalDisableMenuItemTitleTooltip={true}
+            >
+              {/* <Menu.Item
+                key="0"
+                onClick={() => {
+                //   LogOut();
+			alert("log out")
+                }}
+              >
+                <LogoutOutlined />
+              </Menu.Item> */}
+              <Menu.Item key="1" onClick={() => navigate('dashboard')}>
+                <HomeOutlined />
+              </Menu.Item>
+
+              <Menu.Item key="2" onClick={() => navigate('classroom')}>
+                <AppstoreOutlined />
+              </Menu.Item>
+
+              <Menu.Item key="5" onClick={() => navigate('account')}>
+                <UserOutlined />
+              </Menu.Item>
+            </Menu>
+          </div>
+        </Header>
+        <Layout>
+          <Content>
+            <div className="Layout">
+              <Outlet />
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  );
+};
+
+export default UserLayout;
--- ui/src/routes/routes.tsx
+++ ui/src/routes/routes.tsx
@@ -1,1 +1,83 @@
 import React from 'react';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { GuardEC } from '../utils/models/Guard';
+import Guard from '../guards/AuthGuard';
+import { CookiesProvider } from 'react-cookie';
+import UserLayout from '../pages/Layout/UserLayout';
+import ClassroomLayout from '../pages/Layout/ClassroomLayout';
+import Test from '../pages/Test';
+import { AdminEC, StudentEC } from '../utils/models/Guard';
+
+export default function AppRoute() {
+  const AdminGuard: GuardEC = {
+    guardEntity: AdminEC,
+  };
+
+  const StudentGuard: GuardEC = {
+    guardEntity: StudentEC,
+  };
+
+  return (
+    <BrowserRouter>
+      <CookiesProvider>
+        <Routes>
+          {/* public routes */}
+          {/* <Route path="/" element={<Login />} /> */}
+
+          {/* student routes */}
+          <Route path="/student/*">
+            <Route element={<UserLayout />}>
+              <Route path="dashboard/*">
+                <Route path="*" element={<Navigate to="" replace />} />
+                <Route path="home" element={<Test />} />
+              </Route>
+
+              <Route path="classroom/*">
+                <Route element={<ClassroomLayout />}>
+                  <Route path="" element={<Test />} />
+                  {/* <Route path="" element={<Classroom />} /> */}
+                  {/* <Route path=":id/home" element={<Classroom />} /> */}
+                  {/* <Route path=":id/test" element={<Test />} /> */}
+                </Route>
+
+                <Route path="*" element={<Navigate to="" replace />} />
+              </Route>
+              {/* <Route path="account/*" element={<Account />} /> */}
+            </Route>
+
+            {/* add new user routes here */}
+            <Route path="*" element={<Navigate to="dashboard" replace />} />
+          </Route>
+
+          {/* admin routes */}
+          <Route path="/admin/*" element={<Guard {...AdminGuard} />}>
+            <Route element={<UserLayout />}>
+              {/* <Route path="dashboard/*">
+		      <Route path="" element={<AdminDashboardLayout />} />
+		      <Route path="usercontrol" element={<ControlLayout />} />
+		    </Route> */}
+
+              <Route path="classroom/*">
+                <Route path="" element={<Test />} />
+                <Route element={<ClassroomLayout />}>
+                  {/* <Route path="" element={<Classroom />} /> */}
+                  {/* <Route path=":id/home" element={<Classroom />} /> */}
+                  {/* <Route path=":id/test" element={<Test />} /> */}
+                </Route>
+                <Route path="*" element={<Navigate to="" replace />} />
+              </Route>
+
+              {/* <Route path="account/*" element={<Account />} /> */}
+              {/* add new admin routes here */}
+              <Route path="*" element={<Navigate to="dashboard/" replace />} />
+            </Route>
+          </Route>
+
+          {/* not found */}
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </CookiesProvider>
+    </BrowserRouter>
+  );
+}
