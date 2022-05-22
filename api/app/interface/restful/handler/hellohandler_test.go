c1cf6a42aaefa17f5024a2142f09a1487b20ec64
Vinh Truong <56877552+thvinhtruong@users.noreply.github.com>
2022-05-22 15:33:57 +0700

    add hello handler and unit test

--- api/app/interface/restful/handler/hellohandler_test.go
+++ api/app/interface/restful/handler/hellohandler_test.go
@@ -1,1 +1,21 @@
 package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+)
+
+func TestHello(t *testing.T) {
+	r := Hello()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/hello", nil)
+
+	r.ServeHTTP(w, req)
+	expected := `{"message":"hello"}`
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, expected, w.Body.String())
+
+}
