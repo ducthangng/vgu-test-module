a362a7393f26d41c3df822f4b2d2f029aed500a6
Vinh Truong <56877552+thvinhtruong@users.noreply.github.com>
2022-05-22 16:23:53 +0700

    complete the logger middleware

--- api/app/interface/restful/appmiddleware/logger.go
+++ api/app/interface/restful/appmiddleware/logger.go
@@ -1,19 +1,38 @@
 package middleware
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+func saveLog(name string, s ...string) {
+	file, err := os.Create("../appmiddleware/log/" + name + ".log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	for _, items := range s {
+		file.WriteString(items + " ")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d bytes written\n", len(s))
+	gin.DefaultWriter = file
+}
+
 func Logger() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		// print time
 		t := time.Now()
-		log.Println("Request started at", t.Format("2006-01-02 15:04:05"))
+		name := t.Format("2006-01-02")
+		started := t.Format("2006-01-02 15:04:05")
 
-		// before request
 		// executes pending handlers in the chain inside the calling handler.
 		ctx.Next()
 		// after request
@@ -25,8 +44,9 @@ func Logger() gin.HandlerFunc {
 		// print method used
 		method := ctx.Request.Method
 
-		// print
-		log.Printf("%s on %s with status %d which takes %s", method, ctx.Request.URL.Path, status, latency)
+		// save to log file
+		saveLog(name, started, latency.String(), strconv.Itoa(status), method,
+			ctx.Request.URL.Path, ctx.ClientIP(), ctx.Request.UserAgent())
 
 	}
 }
