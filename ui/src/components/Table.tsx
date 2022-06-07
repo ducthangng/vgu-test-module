af64f355780390047db0cf7e780a385921f5975f
Linh Ngo Phuc <phuclinh9090@gmail.com>
2022-06-07 22:19:05 +0700

    Update Table component version 3

--- ui/src/components/Table.tsx
+++ ui/src/components/Table.tsx
@@ -1,167 +1,87 @@
-import React, { useState } from 'react';
-import { Table, Switch, Radio, Form, Space } from 'antd';
+import React from 'react';
+import { Table, Button } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+// import { RouterComponentProps } from "react-router";
 
-const columns = [
-  {
-    title: 'userName',
-    dataIndex: 'username',
-  },
-  {
-    title: 'Gender',
-    dataIndex: 'gender',
-    filters: [
-      { text: 'Male', value: 'male' },
-      { text: 'Female', value: 'female' },
-    ],
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    sorter: true,
-    render: () => (
-      <Space size="middle">
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
-const data = [];
+type Props = {};
 
-const App = () => {
-  const [bordered, setBordered] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [size, setSize] = useState('middle');
-  const [showHeader, setShowHeader] = useState(true);
-  const [hasData, setHasData] = useState(true);
-  const [tableLayout, setTableLayout] = useState(undefined);
-  const [bottom, setBottom] = useState('bottomCenter');
-  const [yScroll, setYScroll] = useState(false);
-  const [xScroll, setXScroll] = useState(undefined);
+type User = {
+  name: string;
+  Email: string;
+  userId: number;
+};
 
-  const handleBorderChange = (enable: any) => {
-    setBordered(enable);
+const DataTable: React.FC<Props> = () => {
+  //GOTO: Call to delete user information from database
+  const onDelete = (userId: number) => {
+    console.log('Delete id: ', userId);
   };
 
-  const handleLoadingChange = (enable: any) => {
-    setLoading(enable);
-  };
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Hai',
+      Email: 'hai@gmail.com',
+      userId: 1,
+    },
+    {
+      key: '2',
+      name: 'John',
+      Email: 'john@gmail.com',
+      userId: 2,
+    },
+    {
+      key: '3',
+      name: 'John',
+      Email: 'john@gmail.com',
+      userId: 3,
+    },
+  ];
 
-  const handleSizeChange = (e: any) => {
-    setSize(e.target.value);
-  };
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'User ID',
+      dataIndex: 'userID',
+      key: 'userID',
+    },
+    /*
+    {
+      title: "Action",
+      key: "action",
+      render: (action: any , record: User) => (
+        return (
+          <>
+          <Button type="primary"
+                  icon="delete"
+                  onClick={() => onDelete()}>
+            Delete 
+          </Button>
+          </>
+        )
+      ),
+    },
+*/
+  ];
 
-  const handleTableLayoutChange = (e: any) => {
-    setTableLayout(e.target.value);
-  };
-
-  const handleHeaderChange = (enable: any) => {
-    setShowHeader(enable);
-  };
-
-  const handleYScrollChange = (enable: any) => {
-    setYScroll(enable);
-  };
-
-  const handleXScrollChange = (e: any) => {
-    setXScroll(e.target.value);
-  };
-
-  const handleDataChange = (newHasData: any) => {
-    setHasData(newHasData);
-  };
-
-  const scroll = {};
-
-  if (yScroll) {
-    scroll.y = 240;
-  }
-
-  if (xScroll) {
-    scroll.x = '100vw';
-  }
-
-  const tableColumns = columns.map((item) => ({ ...item, ellipsis }));
-
-  if (xScroll === 'fixed') {
-    tableColumns[0].fixed = true;
-    tableColumns[tableColumns.length - 1].fixed = 'right';
-  }
-
-  const tableProps = {
-    bordered,
-    loading,
-    size,
-
-    scroll,
-    tableLayout,
-  };
   return (
-    <>
-      <Form
-        layout="inline"
-        className="components-table-demo-control-bar"
-        style={{
-          marginBottom: 16,
-        }}
-      >
-        <Form.Item label="Bordered">
-          <Switch checked={bordered} onChange={handleBorderChange} />
-        </Form.Item>
-        <Form.Item label="loading">
-          <Switch checked={loading} onChange={handleLoadingChange} />
-        </Form.Item>
-        <Form.Item label="Column Header">
-          <Switch checked={showHeader} onChange={handleHeaderChange} />
-        </Form.Item>
-        <Form.Item label="Has Data">
-          <Switch checked={!!hasData} onChange={handleDataChange} />
-        </Form.Item>
-        <Form.Item label="Size">
-          <Radio.Group value={size} onChange={handleSizeChange}>
-            <Radio.Button value="large">Large</Radio.Button>
-            <Radio.Button value="middle">Middle</Radio.Button>
-            <Radio.Button value="small">Small</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Table Scroll">
-          <Radio.Group value={xScroll} onChange={handleXScrollChange}>
-            <Radio.Button value={undefined}>Unset</Radio.Button>
-            <Radio.Button value="scroll">Scroll</Radio.Button>
-            <Radio.Button value="fixed">Fixed Columns</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Table Layout">
-          <Radio.Group value={tableLayout} onChange={handleTableLayoutChange}>
-            <Radio.Button value={undefined}>Unset</Radio.Button>
-            <Radio.Button value="fixed">Fixed</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Pagination Bottom">
-          <Radio.Group
-            value={bottom}
-            onChange={(e) => {
-              setBottom(e.target.value);
-            }}
-          >
-            <Radio.Button value="bottomCenter">BottomCenter</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-      </Form>
-      <Table
-        {...tableProps}
-        pagination={{
-          position: [bottom],
-        }}
-        columns={tableColumns}
-        dataSource={hasData ? data : []}
-        scroll={scroll}
-      />
-    </>
+    <div style={{ padding: 24, background: '#fff', minHeight: '360' }}>
+      <span>
+        <Table dataSource={dataSource} columns={columns}>
+          ;
+        </Table>
+      </span>
+    </div>
   );
 };
 
-export default App;
+export default DataTable;
