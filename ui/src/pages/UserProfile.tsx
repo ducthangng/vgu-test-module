4eaee77374b97243c14ea7a59f48c839210195fc
Linh Ngo Phuc <phuclinh9090@gmail.com>
2022-06-13 12:24:32 +0700

    Create User Profile Page

---
+++ ui/src/pages/UserProfile.tsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from 'react';
+// import {useHistory} from 'react-router-dom';
+
+import UserChart from '../components/UserProfile/UserChart';
+import UserProfileCard from '../components/UserFrofile/UserProfileCard';
