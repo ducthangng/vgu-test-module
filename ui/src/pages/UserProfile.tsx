88ccf57125c87afbff0aaa9582581a0135ca4d26
Linh Ngo Phuc <phuclinh9090@gmail.com>
2022-06-13 12:42:41 +0700

    Create User Profile Page and merge User Chart, User Profile Data

--- ui/src/pages/UserProfile.tsx
+++ ui/src/pages/UserProfile.tsx
@@ -3,3 +3,40 @@ import React, { useEffect, useState } from 'react';
 
 import UserChart from '../components/UserProfile/UserChart';
 import UserProfileCard from '../components/UserFrofile/UserProfileCard';
+
+import { Layout, Divider } from 'antd';
+
+const { Sider, Content } = Layout;
+
+function UserProfile() {
+  return (
+    <div style={{ width: '65%', display: 'flex', justifyContent: 'center' }}>
+      <body>
+        <Divider
+          orientation="left"
+          style={{ fontSize: '25px', fontFamily: 'Roboto' }}
+        >
+          User Profile
+        </Divider>
+        <Layout>
+          <Sider>
+            <UserProfileCard
+              id={0}
+              fullname={''}
+              email={''}
+              phone={''}
+              address={''}
+              dob={''}
+              avatar={undefined}
+            ></UserProfileCard>
+          </Sider>
+          <Content>
+            <UserChart></UserChart>
+          </Content>
+        </Layout>
+      </body>
+    </div>
+  );
+}
+
+export default UserProfile;
