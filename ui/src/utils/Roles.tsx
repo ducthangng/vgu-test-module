0c52936601d95ae58dbfff0e7066a14c7b69c621
nhatquang101 <16141@student.vgu.edu.vn>
2022-06-14 18:13:22 +0700

    add logout and roles functions

--- ui/src/utils/Roles.tsx
+++ ui/src/utils/Roles.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React from "react";
+import { authApi } from "../api/authApi";
+import { useNavigate } from "react-router-dom";
 
 export const roleFunc = {
-  getRole: (parameter: { EC: number }) => {
-    const ec = {
-      data: parameter.EC,
-    };
+  getRole: async () => {
+    const Role = await authApi
+      .validateRole()
+      .then((EC) => {
+        console.log("Roles here: EC ", EC);
+        console.log("Type of EC: ", typeof EC);
+        switch (EC) {
+          case 1:
+            return "admin";
+          case 3:
+            return "student";
+          case 2:
+            return "teacher";
+          default:
+            throw new Error("EC does not supported");
+        }
+      })
+      .then((role) => {
+        return role;
+      });
 
-    switch (ec.data) {
-      case 1:
-        return 'admin';
-      case 2:
-        return 'student';
-      case 3:
-        return 'teacher';
-      default:
-        throw new Error('EC does not supported');
-    }
+    return Role;
   },
 };
