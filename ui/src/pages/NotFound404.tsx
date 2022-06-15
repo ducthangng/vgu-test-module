034fdc5c2bf5ca79eb61afc81693fa8052523173
Linh Ngo Phuc <phuclinh9090@gmail.com>
2022-06-15 21:49:00 +0700

    Edit css and layout

--- ui/src/pages/NotFound404.tsx
+++ ui/src/pages/NotFound404.tsx
@@ -26,7 +26,7 @@ export default function NotFound404() {
             onClick={handleHome}
             className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-primary active:bg-primary hover:bg-primary/70"
           >
-            back to homepage
+            Back to homepage
           </button>
         </div>
         <div className="max-w-lg">
