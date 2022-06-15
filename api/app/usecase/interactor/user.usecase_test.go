9fc8989feb16f0bcccbef6e09af3ed7ea0afdb8f
nhatquang101 <16141@student.vgu.edu.vn>
2022-06-15 10:05:22 +0700

    Set up test for user usecase

---
+++ api/app/usecase/interactor/user.usecase_test.go
@@ -0,0 +1,25 @@
+package interactor
+
+import "testing"
+
+func TestCreateUser(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestUpdateUser(t *testing.T) {
+	t.Skip("Not implemented")
+
+}
+
+func TestFindUser(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestFindUserClasses(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestReviewTestResults(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
