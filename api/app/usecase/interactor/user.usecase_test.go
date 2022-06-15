f582dd8284ed257f139f78c691f86e04ba593542
nhatquang101 <16141@student.vgu.edu.vn>
2022-06-15 11:39:33 +0700

    Complete test for user use case

--- api/app/usecase/interactor/user.usecase_test.go
+++ api/app/usecase/interactor/user.usecase_test.go
@@ -143,14 +143,194 @@ func TestUpdateUser(t *testing.T) {
 }
 
 func TestFindUser(t *testing.T) {
-	t.Skip("Not implemented")
+	type args struct {
+		ctx         context.Context
+		user        usecase_dto.User
+		hasPassword bool
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    []usecase_dto.User
+		wantErr error
+	}{
+		{
+			name: "FindUserSuccess",
+			args: args{
+				ctx:         context.Background(),
+				user:        usecase_dto.User{},
+				hasPassword: true,
+			},
+			want:    []usecase_dto.User{},
+			wantErr: nil,
+		},
+
+		{
+			name: "FindUserError",
+			args: args{
+				ctx:         context.Background(),
+				user:        usecase_dto.User{},
+				hasPassword: true,
+			},
+			want:    []usecase_dto.User{},
+			wantErr: errors.New("can not find user"),
+		},
+	}
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRepo := mockusecase.NewMockUserUseCase(mockCtrl)
+
+	for _, tt := range tests {
+		if tt.name == "FindUserSuccess" {
+			t.Run(tt.name, func(t *testing.T) {
+				mockRepo.EXPECT().FindUser(tt.args.ctx, tt.args.user, tt.args.hasPassword).Return(tt.want, nil)
+				userusecase := buildUserTest()
+				got, err := userusecase.FindUser(tt.args.ctx, tt.args.user, tt.args.hasPassword)
+				if err != nil {
+					t.Errorf("UserUsecase.FindUser() error = %v", err)
+				}
+
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("FindUser() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+
+		if tt.name == "FindUserError" {
+			t.Run(tt.name, func(t *testing.T) {
+				mockRepo.EXPECT().FindUser(tt.args.ctx, tt.args.user, tt.args.hasPassword).Return(tt.want, tt.wantErr)
+				userusecase := buildUserTest()
+				got, err := userusecase.FindUser(tt.args.ctx, tt.args.user, tt.args.hasPassword)
+				if err != tt.wantErr {
+					t.Errorf("UserUsecase.FindUser() error = %v, want %v", err, tt.wantErr)
+				}
+
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("FindUser() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+	}
 }
 
 func TestFindUserClasses(t *testing.T) {
-	t.Skip("Not implemented")
+	type args struct {
+		ctx    context.Context
+		userId int
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    []usecase_dto.Class
+		wantErr error
+	}{
+		{
+			name: "FindUserClassesSuccess",
+			args: args{
+				ctx:    context.Background(),
+				userId: 1,
+			},
+			want:    []usecase_dto.Class{},
+			wantErr: nil,
+		},
+
+		{
+			name: "FindUserClassesError",
+			args: args{
+				ctx:    context.Background(),
+				userId: 1,
+			},
+			want:    []usecase_dto.Class{},
+			wantErr: errors.New("can not find user classes"),
+		},
+	}
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRepo := mockusecase.NewMockUserUseCase(mockCtrl)
+
+	for _, tt := range tests {
+		if tt.name == "FindUserClassesSuccess" {
+			t.Run(tt.name, func(t *testing.T) {
+				mockRepo.EXPECT().FindUserClasses(tt.args.ctx, tt.args.userId).Return(tt.want, nil)
+				userusecase := buildUserTest()
+				got, err := userusecase.FindUserClasses(tt.args.ctx, tt.args.userId)
+				if err != nil {
+					t.Errorf("UserUsecase.FindUserClasses() error = %v", err)
+				}
+
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("FindUserClasses() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+
+		if tt.name == "FindUserClassesError" {
+			t.Run(tt.name, func(t *testing.T) {
+				mockRepo.EXPECT().FindUserClasses(tt.args.ctx, tt.args.userId).Return(tt.want, tt.wantErr)
+				userusecase := buildUserTest()
+				got, err := userusecase.FindUserClasses(tt.args.ctx, tt.args.userId)
+				if err != tt.wantErr {
+					t.Errorf("UserUsecase.FindUserClasses() error = %v, want %v", err, tt.wantErr)
+				}
+
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("FindUserClasses() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+	}
 }
 
 func TestReviewTestResults(t *testing.T) {
-	t.Skip("Not implemented")
+	type args struct {
+		ctx      context.Context
+		resultId int
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    usecase_dto.SkillTest
+		wantErr error
+	}{
+		{
+			name: "ReviewTestResults",
+			args: args{
+				ctx:      context.Background(),
+				resultId: 1,
+			},
+			want:    usecase_dto.SkillTest{},
+			wantErr: nil,
+		},
+	}
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRepo := mockusecase.NewMockUserUseCase(mockCtrl)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo.EXPECT().ReviewTestResult(tt.args.ctx, tt.args.resultId).Return(tt.want, tt.wantErr)
+			userusecase := buildUserTest()
+			got, err := userusecase.ReviewTestResult(tt.args.ctx, tt.args.resultId)
+
+			if err != tt.wantErr {
+				t.Errorf("UserUsecase.ReviewTestResults() error = %v, want %v", err, tt.wantErr)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReviewTestResults() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
 }
+
 
