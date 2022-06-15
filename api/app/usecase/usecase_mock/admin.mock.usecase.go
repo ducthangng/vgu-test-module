636afd5cdaaf640460a1ec3fb035dcd3b3e12d6c
nhatquang101 <16141@student.vgu.edu.vn>
2022-06-15 13:17:02 +0700

    Complete admin mock use case

--- api/app/usecase/usecase_mock/admin.mock.usecase.go
+++ api/app/usecase/usecase_mock/admin.mock.usecase.go
@@ -18,3 +18,35 @@ func NewMockAdminUseCase(ctrl *gomock.Controller) *MockAdminUseCase {
 func (m *MockAdminUseCase) EXPECT() *MockAdminUseCaseMockRecorder {
 	return m.recorder
 }
+
+func (m *MockAdminUseCase) UpdateUserTestResult(ctx context.Context, testResult usecase_dto.TestResult) (err error){
+	ret := m.ctrl.Call(m, "UpdateUserTestResult", ctx, testResult)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+func (m *MockAdminUseCaseMockRecorder) UpdateUserTestResult(ctx context.Context, testResult interface{}) *gomock.Call {
+	return m.mock.ctrl.RecordCallWithMethodType(m.mock, "UpdateUserTestResult", reflect.TypeOf((*MockAdminUseCase)(nil).UpdateUserTestResult), ctx, testResult)
+}
+
+func (m *MockAdminUseCase) DeleteUserTestResult(ctx context.Context, id int) (err error){
+	ret := m.ctrl.Call(m, "DeleteUserTestResult", ctx, id)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+func (m *MockAdminUseCaseMockRecorder) DeleteUserTestResult(ctx context.Context, id interface{}) *gomock.Call {
+	return m.mock.ctrl.RecordCallWithMethodType(m.mock, "DeleteUserTestResult", reflect.TypeOf((*MockAdminUseCase)(nil).DeleteUserTestResult), ctx, id)
+}
+
+func (m *MockAdminUseCase) DeleteUser(ctx context.Context, id int) (err error){
+	ret := m.ctrl.Call(m, "DeleteUser", ctx, id)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+func (m *MockAdminUseCaseMockRecorder) DeleteUser(ctx context.Context, id interface{}) *gomock.Call {
+	return m.mock.ctrl.RecordCallWithMethodType(m.mock, "DeleteUser", reflect.TypeOf((*MockAdminUseCase)(nil).DeleteUser), ctx, id)
+}
+
+
