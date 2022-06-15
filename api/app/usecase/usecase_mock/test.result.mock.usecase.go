5ace6fa501461f128feac20aa86604a45a85f1a3
nhatquang101 <16141@student.vgu.edu.vn>
2022-06-15 13:08:57 +0700

    Complete test result mock use case

--- api/app/usecase/usecase_mock/test.result.mock.usecase.go
+++ api/app/usecase/usecase_mock/test.result.mock.usecase.go
@@ -18,3 +18,36 @@ func NewMockTestResultUseCase(ctrl *gomock.Controller) *MockTestResultUseCase {
 func (m *MockTestResultUseCase) EXPECT() *MockTestResultUseCaseMockRecorder {
 	return m.recorder
 }
+
+func (m *MockTestResultUseCase) GetUserTestResults(ctx context.Context, userID int) (results []usecase_dto.TestResult, err error) {
+	ret := m.ctrl.Call(m, "GetUserTestResults", ctx, userID)
+	ret0, _ := ret[0].([]usecase_dto.TestResult)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+func (m *MockTestResultUseCaseMockRecorder) GetUserTestResults(ctx context.Context, userID interface{}) *gomock.Call {
+	return m.mock.ctrl.RecordCallWithMethodType(m.mock, "GetUserTestResults", reflect.TypeOf((*MockTestResultUseCase)(nil).GetUserTestResults), ctx, userID)
+}
+
+func (m *MockTestResultUseCase) GetUserTestResultDetail(ctx context.Context, testId int) (result usecase_dto.TestResult, err error){
+	ret := m.ctrl.Call(m, "GetUserTestResultDetail", ctx, testId)
+	ret0, _ := ret[0].(usecase_dto.TestResult)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+func (m *MockTestResultUseCaseMockRecorder) GetUserTestResultDetail(ctx context.Context, testId interface{}) *gomock.Call {
+	return m.mock.ctrl.RecordCallWithMethodType(m.mock, "GetUserTestResultDetail", reflect.TypeOf((*MockTestResultUseCase)(nil).GetUserTestResultDetail), ctx, testId)
+}
+
+func (m *MockTestResultUseCase) GetTestResultHeadline(ctx context.Context, testResultId int) (result usecase_dto.TestResult, err error){
+	ret := m.ctrl.Call(m, "GetTestResultHeadline", ctx, testResultId)
+	ret0, _ := ret[0].(usecase_dto.TestResult)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+func (m *MockTestResultUseCaseMockRecorder) GetTestResultHeadline(ctx context.Context, testResultId interface{}) *gomock.Call {
+	return m.mock.ctrl.RecordCallWithMethodType(m.mock, "GetTestResultHeadline", reflect.TypeOf((*MockTestResultUseCase)(nil).GetTestResultHeadline), ctx, testResultId)
+}
