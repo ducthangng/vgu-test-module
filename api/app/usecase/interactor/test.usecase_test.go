a48e1e6194fc26db60bf8da42d42e25b7fefb686
Nguyen Duy Phu Quang <15890@student.vgu.edu.vn>
2022-06-21 16:32:17 +0700

    complete unit test for test usecase

--- api/app/usecase/interactor/test.usecase_test.go
+++ api/app/usecase/interactor/test.usecase_test.go
@@ -87,3 +87,134 @@ func TestQueryInfor(t *testing.T) {
 		}
 	}
 }
+
+func TestQuerySkillTest(t *testing.T) {
+	type args struct {
+		ctx    context.Context
+		testId int
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    usecase_dto.Test
+		wantErr error
+	}{
+		{
+			name: "QuerySkillTest",
+			args: args{
+				ctx:    context.Background(),
+				testId: 1,
+			},
+			want:    usecase_dto.Test{},
+			wantErr: nil,
+		},
+	}
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRepo := mockusecase.NewMockTestUseCase(mockCtrl)
+	testusecase := buildTestModuleTest()
+
+	for _, tt := range tests {
+		if tt.name == "QuerySkillTest" {
+			t.Run(tt.name, func(t *testing.T) {
+				mockRepo.EXPECT().QuerySkillTest(tt.args.ctx, tt.args.testId).Return(tt.want, tt.wantErr)
+				got, err := testusecase.QuerySkillTest(tt.args.ctx, tt.args.testId)
+				if err != nil {
+					t.Errorf("TestUsecase.QuerySkillTest() error = %v", err)
+				}
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("TestUsecase.QuerySkillTest() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+	}
+}
+
+func TestSubmitTest(t *testing.T) {
+	type args struct {
+		ctx        context.Context
+		data       usecase_dto.SubmitData
+		userId     int
+		entityCode int
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    usecase_dto.Test
+		wantErr error
+	}{
+		{
+			name: "SubmitTest",
+			args: args{
+				ctx:    context.Background(),
+				data:   usecase_dto.SubmitData{},
+				userId: 1,
+			},
+			want:    usecase_dto.Test{},
+			wantErr: nil,
+		},
+	}
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRepo := mockusecase.NewMockTestUseCase(mockCtrl)
+	testusecase := buildTestModuleTest()
+
+	for _, tt := range tests {
+		if tt.name == "SubmitTest" {
+			t.Run(tt.name, func(t *testing.T) {
+				mockRepo.EXPECT().SubmitTest(tt.args.ctx, tt.args.data, tt.args.userId, tt.args.entityCode).Return(tt.want, tt.wantErr)
+				got, err := testusecase.SubmitTest(tt.args.ctx, tt.args.data, tt.args.userId, tt.args.entityCode)
+				if err != nil {
+					t.Errorf("TestUsecase.SubmitTest() error = %v", err)
+				}
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("TestUsecase.SubmitTest() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+	}
+}
+
+func TestQueryAllTest(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRepo := mockusecase.NewMockTestUseCase(mockCtrl)
+	testusecase := buildTestModuleTest()
+
+	t.Run("Query All Test", func(t *testing.T) {
+		mockRepo.EXPECT().QueryAllTest(context.Background()).Return([]usecase_dto.Test{}, nil)
+		got, err := testusecase.QueryAllTest(context.Background())
+		if err != nil {
+			t.Errorf("TestUsecase.QueryAllTest() error = %v", err)
+		}
+		if !reflect.DeepEqual(got, []usecase_dto.Test{}) {
+			t.Errorf("TestUsecase.QueryAllTest() = %v, want %v", got, []usecase_dto.Test{})
+		}
+	})
+}
+
+func QueryTestAnswer(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRepo := mockusecase.NewMockTestUseCase(mockCtrl)
+	testusecase := buildTestModuleTest()
+
+	t.Run("Query Test Answer", func(t *testing.T) {
+		mockRepo.EXPECT().QueryTestAnswer(context.Background(), 1).Return([]usecase_dto.SubmitData{}, nil)
+		got, err := testusecase.QueryTestAnswer(context.Background(), 1)
+		if err != nil {
+			t.Errorf("TestUsecase.QueryTestAnswer() error = %v", err)
+		}
+		if !reflect.DeepEqual(got, []usecase_dto.SubmitData{}) {
+			t.Errorf("TestUsecase.QueryTestAnswer() = %v, want %v", got, []usecase_dto.SubmitData{})
+		}
+	})
+}
