52ac3463246dcfc2dcfb08296af41be87756783a
Nguyen Duy Phu Quang <15890@student.vgu.edu.vn>
2022-06-21 17:25:45 +0700

    unit test for admin usecase

--- api/app/usecase/interactor/admin.usecase_test.go
+++ api/app/usecase/interactor/admin.usecase_test.go
@@ -1,9 +1,36 @@
 package interactor
 
-import "testing"
+import (
+	"context"
+	"server/app/domain/repository"
+	"testing"
 
-func TestUpdateTestResult(t *testing.T) {
-	t.Skip("Not implemented")
+	"server/app/usecase/usecase_dto"
+	mockusecase "server/app/usecase/usecase_mock"
+
+	"github.com/golang/mock/gomock"
+)
+
+func buildAdminTest() *AdminUsecase {
+	var repo repository.DataService
+	adminusecase := NewAdminUseCase(repo)
+	return adminusecase
+}
+
+func TestUpdateUserTestResult(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockRepo := mockusecase.NewMockAdminUseCase(mockCtrl)
+	adminusecase := buildAdminTest()
+
+	t.Run("Update Test Result", func(t *testing.T) {
+		mockRepo.EXPECT().UpdateUserTestResult(context.Background(), usecase_dto.TestResult{}).Return(nil)
+		err := adminusecase.UpdateUserTestResult(context.Background(), usecase_dto.TestResult{})
+		if err != nil {
+			t.Errorf("UpdateTestResult() error = %v", err)
+		}
+	})
 }
 
 func TestDeleteUserTestResult(t *testing.T) {
