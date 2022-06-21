2a90441d7453e9cd2e7a6567af70103ab9e50815
Nguyen Duy Phu Quang <15890@student.vgu.edu.vn>
2022-06-21 16:56:11 +0700

    setup test for test result

---
+++ api/app/usecase/interactor/test.result.usecase_test.go
@@ -0,0 +1,15 @@
+package interactor
+
+import "testing"
+
+func TestGetUserTestResult(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestGetUserTestResultDetail(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestGetTestResultHeadline(t *testing.T) {
+	t.Skip("Not implemented")
+}
