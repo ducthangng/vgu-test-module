c866e259e99c88b63cff727df90827527763c647
Nguyen Duy Phu Quang <15890@student.vgu.edu.vn>
2022-06-21 16:57:08 +0700

    setup test for admin usecase

---
+++ api/app/usecase/interactor/admin.usecase_test.go
@@ -0,0 +1,15 @@
+package interactor
+
+import "testing"
+
+func TestUpdateTestResult(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestDeleteUserTestResult(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestDeleteUser(t *testing.T) {
+	t.Skip("Not implemented")
+}
